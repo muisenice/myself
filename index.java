4711326e7e8cb145af0189ff789309fe19498cda
liujun@zhifei-group.com <liujun@zhifei-group.com>
2025-06-23 14:20:32 +0800

    test

--- index.java
+++ index.java
@@ -13,5 +13,21 @@
 public class index {
     public static void main(String[] args) {
         System.out.println("hello world");
+        int[] arr = {1, 3, 2, 5, 4, 6, 9, 8, 7};
+        selectionSort(arr);
     }
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
 }

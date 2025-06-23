1e16a61a8d20feb6f8773d7042edecc35c17b9c0
liujun@zhifei-group.com <liujun@zhifei-group.com>
2025-06-23 14:22:02 +0800

    my

--- index.java
+++ index.java
@@ -13,9 +13,23 @@
 public class index {
     public static void main(String[] args) {
         System.out.println("hello world");
-        int a = 10;
-        int b = 20;
-        int c = a + b;
-        System.out.println(c);
+        insertionSort(new int[]{1, 3, 2, 5, 4});
     }
+
+    /**
+     * 插入排序
+     * @param arr
+     */
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
 }

ee139ca24b64606b89040db8b2232a6ad4ae14a2
liujun@zhifei-group.com <liujun@zhifei-group.com>
2025-06-23 14:13:17 +0800

    2

--- index.java
+++ index.java
@@ -11,4 +11,11 @@
  * @since 0.1.0
  */
 public class index {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
+    }
 }
